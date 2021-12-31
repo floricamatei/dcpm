132d769adb5bd2cb9139359199cfc97a2fede266
Dan Gabriel Pavel <dan.pavel@altimate.ro>
2021-12-31 15:59:59 +0200

    lucru la timeout

--- Circuit Breaker/book/book-outside/src/main/java/ro/ilearn/dcpm/book/adapter/httpclient/ReviewServiceClient.java
+++ Circuit Breaker/book/book-outside/src/main/java/ro/ilearn/dcpm/book/adapter/httpclient/ReviewServiceClient.java
@@ -1,5 +1,6 @@
 package ro.ilearn.dcpm.book.adapter.httpclient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
